17af4a7d98d248218516d00fb1befe772e92c049
olicendi <lyblyu.popastih@gmail.com>
2025-11-25 21:39:08 +0300

    changes test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [WelcomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
