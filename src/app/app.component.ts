ee2b3be62990ab5bc4a87fb5baab3e74d3b6c7b8
olicendi <lyblyu.popastih@gmail.com>
2025-12-23 19:46:47 +0300

    taiga ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
+import { TuiRoot } from "@taiga-ui/core";
 import { Component } from '@angular/core';
 // import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
 import { CardEditorComponent } from './components/card-editor/card-editor.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CardEditorComponent],
+  imports: [CardEditorComponent, TuiRoot],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,3 +1,5 @@
+import { provideEventPlugins } from "@taiga-ui/event-plugins";
+import { provideAnimations } from "@angular/platform-browser/animations";
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
@@ -6,7 +8,11 @@ import { routes } from './app.routes';
 import { baseUrlInterceptorFn } from './core/services/base-url.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
-  provideRouter(routes),
-  provideHttpClient(withInterceptors([baseUrlInterceptorFn])),]
+      providers: [
+            provideAnimations(),
+            provideZoneChangeDetection({ eventCoalescing: true }),
+            provideRouter(routes),
+            provideHttpClient(withInterceptors([baseUrlInterceptorFn])),
+            provideEventPlugins()
+      ]
 };
